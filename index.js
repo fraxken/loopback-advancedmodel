1edfd98e50b6611e97d991882b7c2d380daa988d
fraxken <gentilhomme.thomas@gmail.com>
2017-11-05 22:20:24 +0100

    Add missing HTTP Methods

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function disableModelMethods(model, methodsToExpose = []) {
 /**
  * HTTP Verbose Sets
  */
-const HTTPValidVerbose = new Set(['get','post','patch','head','delete','put']);
+const HTTPValidVerbose = new Set(['get','post','patch','head','delete','put','trace','options','connect']);
 const HTTPValidType = new Set(['string','boolean','number','object']);
 
 /**
