22d10164cd7f8779d3d76e28563e87e5d4165def
Thomas GENTILHOMME <Thomas.GENTILHOMME@bnpparibas.com>
2017-11-06 11:20:02 +0100

    Add source argument to APIDescriptor.accept

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ class APIDescriptor {
      * @throws {TypeError}
      * @throws {Error}
      */
-    accept({ arg = '' , type = 'string', required = false, description }) {
+    accept({ arg = '' , type = 'string', required = false, description, source }) {
         if('string' !== typeof(arg)) {
             throw new TypeError('arg should be a string');
         }
@@ -228,6 +228,9 @@ class APIDescriptor {
         const index = this._descriptor.accepts.push({arg,type,required});
         if('string' === typeof(description)) {
             this._descriptor.accepts[index] = description;
+        }
+        if('string' === typeof(source)) {
+            this._descriptor.accepts[index].http = {source};
         }
         return this;
     }
